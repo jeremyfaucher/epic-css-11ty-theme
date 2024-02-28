4bd722268edbc0b599e391b84a9c1fc656a1b0ab
Jeremy Faucher (MINDTREE LIMITED) <v-jfaucher@microsoft.com>
2024-02-27 20:49:31 -0800

    v0.3.0 update

--- epicConfig.js
+++ epicConfig.js
@@ -1,7 +1,8 @@
 module.exports = {
-    // project source folder where html, nunjucks or php files are
+    // project source folder where html, nunjucks or php files are from root
     projectDir: './src',
-    // where epic
+    // where your epic css theme live from root
     epicThemeDir: './src/my-epic-css',
+    // where you project style.css will get output to from root (name style.css as needed)
     projectStyleDir: './src/_includes/style.css'
 };
